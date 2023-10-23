996a6fc6c4376931c3a46f89a2b642856aa62cee
mazpeng <mazgpeng@gmail.com>
2023-10-23 22:40:05 +0700

    ~

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="about">
+    <div class="card" style="width: 18rem">
+      <img src="#" class="card-img-top" alt="#" />
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
     <h1>This is an about page</h1>
   </div>
-  <v-card title="Card title" subtitle="Subtitle" text="..." variant="tonal">
+  <v-card title="Card title" subtitle="Subtitle" text="#" variant="tonal">
     <v-card-actions>
       <v-btn>Click me</v-btn>
     </v-card-actions>
